2ec77a2a28ee4067b7fe0505ebde910764513023
Stranger <72191000+Geralt-bit@users.noreply.github.com>
2024-05-01 14:21:41 +0600

    Update decompression.cpp

--- decompression.cpp
+++ decompression.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-#include <fstream>
 
-int main() {
-    ifstream compressedFile("compressed.txt");
-    ofstream decompressedFile("decompressed.txt");
-
-    // Close files
-    compressedFile.close();
-    decompressedFile.close();
-
-    return 0;
-}
