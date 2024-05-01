540fc5c36e32c56049911984b8eb6183e449546c
Stranger <72191000+Geralt-bit@users.noreply.github.com>
2024-05-01 14:22:52 +0600

    Update compression.cpp

--- compression.cpp
+++ compression.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-#include <fstream>
-int main() {
-    ifstream inputFile("input.txt");
-    ofstream compressedFile("compressed.txt");
 
-    // Close files
-    inputFile.close();
-    compressedFile.close();
-
-    return 0;
-}
